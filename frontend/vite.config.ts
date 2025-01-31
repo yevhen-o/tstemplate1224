3e95d3889b80dfd219799793450b3debbf05bf02
Yevhen Oliynyk <i.yevhen.oliynyk@gmail.com>
2025-01-31 19:54:30 +0100

    Use react compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const ReactCompilerConfig = {
+  /* ... */
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       src: "/src",
